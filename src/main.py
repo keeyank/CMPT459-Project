759c0ed2e04f9ff4aacc6b74c86e15b9f3f61d50
keeyank <kazemikeeyan@gmail.com>
2022-10-09 10:52:15 -0700

    initial commit

---
+++ src/main.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from pathlib import Path
+import matplotlib.pyplot as plt
+
+def to_outcome_group(outcome):
+    h = {'Discharged', 'Discharged from hospital', 'Hospitalized',
+            'critical condition', 'discharge', 'discharged'}
+    nh = {'Alive', 'Receiving Treatment', 'Stable', 'Under treatment',
+            'recovering at home 03.03.2020', 'released from quarantine',
+            'stable', 'stable condition'}
+    d = {'Dead', 'Death', 'Deceased', 'Died', 'death', 'died'}
+    r = {'Recovered', 'recovered'}
+
+    if outcome in h: 
+        return 'hospitalized'
+    elif outcome in nh: 
+        return 'nonhospitalized'
+    elif outcome in d: 
+        return 'deceased'
+    elif outcome in r: 
+        return 'recovered'
+
+cases_train = pd.read_csv('../data/cases_2021_train.csv')
+outcomes = cases_train['outcome']
+
+outcome_groups = outcomes.map(to_outcome_group)
+
+cases_train['outcome_group'] = outcome_groups
+
+cases_train.to_csv('./data/cases_2021_train_processed.csv')
+
+#print(cases_train['outcome_group'])
